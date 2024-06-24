8e8a78f8d89e27117c3049ab0e2abbb46186dfe6
far_faqiri <farida.faqiri2@gmail.com>
2024-06-24 15:59:23 +0430

    fis the linters error

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ const checkButton = document.getElementById('check-btn');
 const enterInput = document.getElementById('text-input');
 const result = document.getElementById('result');
 const removeNonAlphaNumeric = (text) => text.replace(/[\W_]/g, '').toLowerCase();
-
-const checkPalindrome = (inputText) => {
+const checkPalindrome = inputText => {
   const cleanedInput = removeNonAlphaNumeric(inputText);
-  const reversedInput = cleanedInput.split('').reverse().join('');
-  return cleanedInput === reversedInput;
-}
+  return cleanedInput === cleanedInput.split('').reverse().join('');
+};
 
 checkButton.addEventListener("click", function() {
   const userInput = enterInput.value;
   if(userInput.trim().length === 0) alert('Please input a value!');
   const isPalindrome = checkPalindrome(userInput);
   if(isPalindrome) {
-    result.innerHTML = `<span class="highlight">${userInput}</span> is a palindrome`;
+    result.innerHTML = `<span class='highlight'>${userInput}</span> is a palindrome`;
   } else {
-    result.innerHTML = `<span class="highlight">${userInput}</span> is not a palindrome`;
+    result.innerHTML = `<span class='highlight'>${userInput}</span> is not a palindrome`;
   }
 });
