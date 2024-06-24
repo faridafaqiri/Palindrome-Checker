3ae4c3f30047884ecb1af2aa8a4decd40e611616
far_faqiri <farida.faqiri2@gmail.com>
2024-06-24 16:18:59 +0430

    sloved the error

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const checkButton = document.getElementById('check-btn');
 const enterInput = document.getElementById('text-input');
 const result = document.getElementById('result');
 const removeNonAlphaNumeric = (text) => text.replace(/[\W_]/g, '').toLowerCase();
-const checkPalindrome = inputText => {
+const checkPalindrome = (inputText) => {
   const cleanedInput = removeNonAlphaNumeric(inputText);
   return cleanedInput === cleanedInput.split('').reverse().join('');
 };
 
 checkButton.addEventListener ('click', function() {
   const userInput = enterInput.value;
-  if (userInput.trim().length === 0) alert('Please input a value!');
+  if(userInput.trim().length === 0){
+    alert('Please input a value!')}
   const isPalindrome = checkPalindrome(userInput);
-  if (isPalindrome) {
+  if(isPalindrome) {
     result.innerHTML = `<span class='highlight'>${userInput}</span> is a palindrome`;
   } else {
     result.innerHTML = `<span class='highlight'>${userInput}</span> is not a palindrome`;
